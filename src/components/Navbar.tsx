ca131ef203a2beae6976f1bd3679461567387966
Alex Gutierrez <gutierrez.lx@gmail.com>
2021-04-23 14:51:43 -0700

    feat(navbar): created navbar component with links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,38 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
-    <div id='navbar'>
-      <div className='row text-center'>
-        <div className='col'>
-          <h2>Navbar</h2>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+      {/* Logo */}
+      <Link to='/' className='navbar-brand'>
+        Shopsy
+      </Link>
+
+      {/* Menu */}
+      <div className='navbar-nav'>
+        <Link className='nav-link' to='/'>
+          Home
+        </Link>
+        <Link className='nav-link' to='/cart'>
+          Cart
+        </Link>
+        <Link className='nav-link' to='/checkout'>
+          Checkout
+        </Link>
+        <Link className='nav-link' to='/products/1'>
+          Product
+        </Link>
+      </div>
+
+      {/* Cart Icon */}
+      <div className='sh-cart-icon'>
+        <div className='navbar-nav'>
+          <Link to='/cart' className='nav-link'>
+            Cart
+          </Link>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
