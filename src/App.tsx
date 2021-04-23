0ed75bf4b0725fa3375a0a052bed3c2b37b347c4
Alex Gutierrez <gutierrez.lx@gmail.com>
2021-04-23 10:49:36 -0700

    chore(env-setup): setting up initial project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className='row text-center'>
+        <div className='col'>
+          <h2>This is my App!</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
