1f04a33fc3e056feeee311d69648a8b31e097269
Alex Gutierrez <gutierrez.lx@gmail.com>
2021-04-23 13:57:18 -0700

    feature(core-components): adding components and pages for app architecture

---
+++ src/pages/ProductPage.tsx
@@ -0,0 +1,13 @@
+const ProductPage = () => {
+  return (
+    <div id='product'>
+      <div className='row text-center'>
+        <div className='col'>
+          <h2>Product Page!</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductPage;
