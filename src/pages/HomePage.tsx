8a76478a908fca362a3b951d8545d75c8f4ea860
Alex Gutierrez <gutierrez.lx@gmail.com>
2021-05-03 14:05:14 -0700

    feat(prod-card): create component for product card and import to home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import ProductCard from '../components/ProductCard';
 import { GlobalContext } from '../context/GlobalContext';
 import '../App.css';
 
@@ -22,34 +22,7 @@ const HomePage = () => {
         {products.map((product, i) => {
           return (
             <div className='col-sm-12 col-md-3 mb-3' key={i}>
-              <div className='card h-100'>
-                {/* Image on Top */}
-                <div className='sh-card-img'>
-                  <div
-                    className='sh-bg-img'
-                    style={{ backgroundImage: `url(${product.image})` }}></div>
-                </div>
-
-                {/* Card Body */}
-                <div className='card-body'>
-                  <h5 className='card-title'>
-                    <Link to={`/products/${product.id}`}>{product.title}</Link>
-                  </h5>
-                  <div className='d-flex justify-content-between align-items-center'>
-                    <strong>${product.price}</strong>
-                    <span className='badge badge-warning'>
-                      {product.category}
-                    </span>
-                  </div>
-                </div>
-
-                {/* Add to Cart Button  */}
-                <div className='card-footer'>
-                  <button className='btn btn-block btn-primary'>
-                    Add To Cart
-                  </button>
-                </div>
-              </div>
+              <ProductCard product={product} />
             </div>
           );
         })}
