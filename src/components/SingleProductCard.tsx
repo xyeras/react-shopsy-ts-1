16980fe899a140fe1f607a590f32cf66a46fcd8e
xyeras <saika_fujimoto@mail.fresnostate.edu>
2021-05-04 09:48:38 -0700

    refact(single-product-card): created component template for SingleCardProduct

---
+++ src/components/SingleProductCard.tsx
@@ -0,0 +1,11 @@
+const SingleProductCard = () => {
+
+    return (
+        <div id="single-product-card">
+            This is my Single Product Card!
+        </div>
+    );
+
+}
+
+export default SingleProductCard;
